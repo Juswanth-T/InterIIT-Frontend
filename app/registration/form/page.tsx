66ad1848efb522a7320af41eaac1078dfbd55ced
unknown <ee21b125@smail.iitm.ac.in>
2023-09-18 11:53:35 +0530

    Changing the styles of the form and preview Page

--- app/registration/form/page.tsx
+++ app/registration/form/page.tsx
@@ -85,9 +85,9 @@ const Home: React.FC = () => {
                 htmlFor={`name${index}`}
                 className="block text-sm font-medium"
               >
-                Name:
               </label>
               <input
+              placeholder="name"
                 type="text"
                 id={`name${index}`}
                 name={`name${index}`}
@@ -95,7 +95,7 @@ const Home: React.FC = () => {
                 onChange={(e) =>
                   handleParticipantChange(index, "name", e.target.value)
                 }
-                className="px-4 py-2 mt-1 border rounded-lg"
+                className="text-black focus:text-white placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"
                 required
               />
             </div>
@@ -104,9 +104,10 @@ const Home: React.FC = () => {
                 htmlFor={`email${index}`}
                 className="block text-sm font-medium"
               >
-                Institute Email Id:
+                
               </label>
               <input
+              placeholder="Institute Email Id"
                 type="email"
                 id={`email${index}`}
                 name={`email${index}`}
@@ -114,18 +115,19 @@ const Home: React.FC = () => {
                 onChange={(e) =>
                   handleParticipantChange(index, "email", e.target.value)
                 }
-                className="px-4 py-2 mt-1 border rounded-lg"
+                className="text-black focus:text-white placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"
                 required
               />
             </div>
+            <div className="grid grid-cols-4">
             <div className="mb-2">
               <label
                 htmlFor={`college${index}`}
                 className="block text-sm font-medium"
               >
-                Phone Number:
               </label>
               <input
+              placeholder="Phone number"
                 type="text"
                 id={`college${index}`}
                 name={`college${index}`}
@@ -133,18 +135,18 @@ const Home: React.FC = () => {
                 onChange={(e) =>
                   handleParticipantChange(index, "college", e.target.value)
                 }
-                className="px-4 py-2 mt-1 border rounded-lg"
+                className="text-black focus:text-white placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"
                 required
               />
             </div>
-            <div className="mb-2">
+            <div className="ml-8 mb-2">
               <label
                 htmlFor={`discord${index}`}
                 className="block text-sm font-medium"
               >
-                Discord ID:
               </label>
               <input
+              placeholder="Discord ID"
                 type="text"
                 id={`discord${index}`}
                 name={`discord${index}`}
@@ -152,36 +154,40 @@ const Home: React.FC = () => {
                 onChange={(e) =>
                   handleParticipantChange(index, "discord", e.target.value)
                 }
-                className="px-4 py-2 mt-1 border rounded-lg"
+                className="text-black focus:text-white placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"
                 required
               />
             </div>
-            <div className="mb-2">
+            <div className="ml-8 mb-2">
               <label
                 htmlFor={`tshirt${index}`}
                 className="block text-sm font-medium"
               >
-                T-Shirt Size:
+                
               </label>
-              <input
-                type="text"
+              <select
                 id={`tshirt${index}`}
                 name={`tshirt${index}`}
                 value={participant.tshirt}
                 onChange={(e) =>
                   handleParticipantChange(index, "tshirt", e.target.value)
                 }
-                className="px-4 py-2 mt-1 border rounded-lg"
+                className="text-gray-600 focus:text-white placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"
                 required
-              />
+              >
+                <option value="" disabled >Select Tshirt Size</option>
+                <option value="Small">Small</option>
+                <option value="Medium">Medium</option>
+                <option value="Large">Large</option>
+                <option value="XL">XL</option>
+              </select>
             </div>
 
-            <div className="mb-2">
+            <div className="ml-8 mb-2">
               <label
                 htmlFor={`food${index}`}
                 className="block text-sm font-medium"
               >
-                Food Preference:
               </label>
               <select
                 id={`food${index}`}
@@ -190,15 +196,17 @@ const Home: React.FC = () => {
                 onChange={(e) =>
                   handleParticipantChange(index, "food", e.target.value)
                 }
-                className="px-4 py-2 mt-1 border rounded-lg"
+                className="text-gray-600 focus:text-white placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"
                 required
               >
-                <option value="None"></option>
+                <option value="" disabled >Select Food Preference</option>
                 <option value="Jain">Jain</option>
                 <option value="Veg">Veg</option>
+                <option value="Non Veg">Non Veg</option>
               </select>
             </div>
-
+            </div>
+            <div className="flex">
             <div className="mb-2">
               <label
                 htmlFor={`photo${index}`}
@@ -219,7 +227,7 @@ const Home: React.FC = () => {
               />
             </div>
 
-            <div className="mb-2">
+            <div className="mb-2 ml-8">
               <label
                 htmlFor={`id${index}`}
                 className="block text-sm font-medium"
@@ -238,6 +246,7 @@ const Home: React.FC = () => {
                 required
               />
             </div>
+            </div>
            {index != 0? <button
                 type="button"
                 onClick={() => handleRemoveParticipant(index)}
@@ -247,21 +256,23 @@ const Home: React.FC = () => {
             </button> : <></>}
           </div>
         ))}
+        <div className="flex">
         {participants.length < MAX_PARTICIPANTS && (
           <button
             type="button"
             onClick={handleAddParticipant}
-            className="px-4 py-2 bg-blue-500 text-white rounded-lg"
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
           >
             Add Participant
           </button>
         )}
         <button
            onClick={handleSubmit}
-          className="px-4 py-2 mt-4 bg-blue-500 text-white rounded-lg"
+           className=" ml-8 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
         >
             Submit
         </button>
+        </div>
       </form>
     </div>
     </>
--- app/registration/form/preview/page.tsx
+++ app/registration/form/preview/page.tsx
@@ -1,33 +1,32 @@
-
 'use client'
 import Navbar from '@/app/registration/components/navbar';
 
-export default function preview(){
-
+export default function Preview() {
   const storedJsonString = localStorage.getItem('myData');
   const participants = JSON.parse(storedJsonString);
 
-return(
+  return (
+    <>
+      <Navbar currentPage='page2' />
+      <div className='flex items-center ml-4 text-3xl mt-8 font-extrabold text-black'>
+  PREVIEW
+</div>
 
-  <>
-  <Navbar currentPage='page2'></Navbar>
-  <div> {participants.map((participant, index) => (
-    <div key={index} className="mb-4">
-      <h3 className="text-lg font-semibold">
-        Participant {index + 1}
-      </h3>
-      <p>Name: {participant.name}</p>
-      <p>Email: {participant.email}</p>
-      <p>College Name: {participant.college}</p>
-      <p>Discord Id: {participant.discord}</p>
-      <p>T-Shirt Size: {participant.tshirt}</p>
-      <p>Food Preference: {participant.food}</p>
-
-    </div>
-  ))}</div>
-
-  </>
-
-)
-
+      <div className="p-4 grid grid-cols-4 gap-4" >
+        {participants.map((participant, index) => (
+          <div key={index} className="mb-4 p-4 border rounded-lg shadow-lg">
+            <h3 className="text-xl font-semibold mb-2">
+              Participant {index + 1}
+            </h3>
+            <p className="text-gray-700">Name: {participant.name}</p>
+            <p className="text-gray-700">Email: {participant.email}</p>
+            <p className="text-gray-700">College Name: {participant.college}</p>
+            <p className="text-gray-700">Discord Id: {participant.discord}</p>
+            <p className="text-gray-700">T-Shirt Size: {participant.tshirt}</p>
+            <p className="text-gray-700">Food Preference: {participant.food}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
